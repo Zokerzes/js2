30db138ca0e059b7b7c9a4882b3cf0fcb95137c0
Zokerzes <yozhik13@gmail.com>
2023-06-23 23:23:49 +0300

    its worked

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ const rightMsgsSnd = document.getElementById('rsend');
 
 leftMsgsSnd.addEventListener('click', function (e) {
   e.preventDefault();
+
   let text = document.querySelector('#leftmsgtext').value;
   AddRow(text);
   document.querySelector("#leftmsgtext").value = "";
+
 })
 
 rightMsgsSnd.addEventListener('click', function (e) {
@@ -18,21 +20,32 @@ rightMsgsSnd.addEventListener('click', function (e) {
 
 
 function AddRow(text) {
-  let area = document.createElement('div');
-  let areaMessage = document.createElement('div');
-  area.className = "areaStyle";
-  areaMessage.className = "areaMessageStyle";
-  areaMessage.innerText = text;
-  area.append(areaMessage);
-  cp.append(area);
+  if (text) {
+    let area = document.createElement('div');
+    let areaMessage = document.createElement('div');
+    area.className = "areaStyle";
+    areaMessage.className = "areaMessageStyle";
+    areaMessage.innerText = text;
+    area.append(areaMessage);
+    cp.append(area);
+    cp.scrollTop = cp.scrollHeight;
+
+  } else {
+
+  }
 }
 
 function AddRowL(text) {
-  let area = document.createElement('div');
-  let areaMessage = document.createElement('div');
-  area.className = "areaStyleL";
-  areaMessage.className = "areaMessageStyleL";
-  areaMessage.innerText = text;
-  area.append(areaMessage);
-  cp.append(area);
+  if (text) {
+    let area = document.createElement('div');
+    let areaMessage = document.createElement('div');
+    area.className = "areaStyleL";
+    areaMessage.className = "areaMessageStyleL";
+    areaMessage.innerText = text;
+    area.append(areaMessage);
+    cp.append(area);
+    cp.scrollTop = cp.scrollHeight;
+  } else {
+
+  }
 }
